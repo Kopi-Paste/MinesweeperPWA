35f32cf2cbb66cae0afa575adb5b2eea0019d07b
Vojtěch Kopal <kopalvo1@fit.cvut.cz>
2022-07-10 14:53:35 +0200

    Implement uncovering around

--- Minesweeper/src/app/minefield/minefield.component.ts
+++ Minesweeper/src/app/minefield/minefield.component.ts
@@ -13,19 +13,26 @@ export class MinefieldComponent implements OnInit {
   height: number;
   mines: number;
 
+  uncoveredTiles: number;
+  flaggedTiles: number;
 
   minesMap: Array<Array<boolean>>;
   minesAround: Array<Array<number>>;
-
+  flagsMap: Array<Array<number>>;
+  uncoveredMap: Array<Array<boolean>>;
 
   constructor() { 
     this.width = 0;
     this.height = 0;
     this.mines = 0;
 
+    this.uncoveredTiles = 0;
+    this.flaggedTiles = 0;
 
     this.minesMap = new Array<Array<boolean>>();
     this.minesAround = new Array<Array<number>>();
+    this.flagsMap = new Array<Array<number>>();
+    this.uncoveredMap = new Array<Array<boolean>>();
   }
 
   static generateRandomNumber(min : number, max : number) : number
@@ -97,6 +104,70 @@ export class MinefieldComponent implements OnInit {
 
       this.placeMines();
       this.countMines();
+
+      this.flagsMap = new Array<Array<number>>(this.height);
+      for (let i = 0; i < this.height; ++i)
+        this.flagsMap[i] = new Array<number>(this.width).fill(0);
+
+      this.uncoveredMap = new Array<Array<boolean>>(this.height);
+      for (let i = 0; i < this.height; ++i)
+        this.uncoveredMap[i] = new Array<boolean>(this.width).fill(false);
+  }
+
+  uncoverDeleteFlag(vertical : number, horizontal : number) : void
+  {
+    if (this.uncoveredMap[vertical][horizontal])
+      return;
+    if (this.flagsMap[vertical][horizontal] == 1)
+      this.flaggedTiles -= 1;
+    this.flagsMap[vertical][horizontal] = 0;
+    this.uncoveredMap[vertical][horizontal] = true;
+    this.uncoveredTiles += 1;
+    if (this.minesAround[vertical][horizontal] == 0)
+      this.uncoverAround(vertical, horizontal);
+  }
+
+  uncoverAround(vertical : number, horizontal : number)
+  {
+    if (vertical != 0 && horizontal != 0)
+      this.uncoverDeleteFlag(vertical - 1, horizontal - 1);
+    if (vertical != 0)
+      this.uncoverDeleteFlag(vertical - 1, horizontal);
+    if (vertical != 0 && horizontal != this.width - 1)
+      this.uncoverDeleteFlag(vertical - 1, horizontal + 1);
+    if (horizontal != 0)
+      this.uncoverDeleteFlag(vertical, horizontal - 1);
+    if (horizontal != this.width - 1)
+      this.uncoverDeleteFlag(vertical, horizontal + 1);
+    if (vertical != this.height - 1 && horizontal != 0)
+      this.uncoverDeleteFlag(vertical + 1, horizontal - 1);
+    if (vertical != this.height - 1)
+      this.uncoverDeleteFlag(vertical + 1, horizontal);
+    if (vertical != this.height - 1 && horizontal != this.width - 1)
+      this.uncoverDeleteFlag(vertical + 1, horizontal + 1);
+  }
+
+  handleUncover(coordinates: string) : void
+  {
+      let coordinatesArr = coordinates.split(" ");
+      let vertical = parseInt(coordinatesArr[0]);
+      let horizontal = parseInt(coordinatesArr[1]);
+      this.uncoveredMap[vertical][horizontal] = true;
+      this.uncoveredTiles += 1;
+      if (this.minesAround[vertical][horizontal] == 0)
+        this.uncoverAround(vertical, horizontal);
+  }
+
+  handleFlag(coordinates: string) : void
+  {
+      let coordinatesArr = coordinates.split(" ");
+      let vertical = parseInt(coordinatesArr[0]);
+      let horizontal = parseInt(coordinatesArr[1]);
+      this.flagsMap[vertical][horizontal] = (this.flagsMap[vertical][horizontal] + 1) % 3;
+      if (this.flagsMap[vertical][horizontal] == 1)
+        this.flaggedTiles += 1;
+      if (this.flagsMap[vertical][horizontal] == 2)
+        this.flaggedTiles -= 1;
   }
 
   ngOnInit(): void {
--- Minesweeper/src/app/tile/tile.component.ts
+++ Minesweeper/src/app/tile/tile.component.ts
@@ -1,32 +1,41 @@
 import { ReturnStatement } from '@angular/compiler';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.css']
 })
-export class TileComponent implements OnInit {
+export class TileComponent implements OnInit, OnChanges {
+
+  @Input() horizontal : number
+
+  @Input() vertical : number
 
   @Input() mine : boolean
 
   @Input() minesAround : number
 
+  @Input() uncovered : boolean;
 
-  uncovered : boolean;
-
-  flagState : number;
+  @Input() flagState : number;
 
   display : string;
 
-  @Output() messageEvent = new EventEmitter<string>();
+  @Output() uncoverEvent = new EventEmitter<string>();
+
+  @Output() flagEvent = new EventEmitter<string>();
+
+  @Output() lossEvent = new EventEmitter<string>();
 
   constructor() {
+    this.horizontal = 0;
+    this.vertical = 0;
     this.mine = false;
     this.minesAround = 0;
     this.uncovered = false;
     this.flagState = 0;
-    this.display = "_";
+    this.display = " ";
    }
 
    updateDisplay()
@@ -37,7 +46,7 @@ export class TileComponent implements OnInit {
         switch (this.flagState)
         {
           case 0:
-            this.display = "_";
+            this.display = " ";
             break;
           case 1:
             this.display = "F";
@@ -48,24 +57,15 @@ export class TileComponent implements OnInit {
         }
    }
 
-  sendMessage()
-  {
-    this.messageEvent.emit("Hey");
-  }
-
   handleLeftClick()
   {
     if (this.uncovered || this.flagState == 1)
       return;
-    this.flagState = 0;
-    this.uncovered = true;
 
     if (this.mine)
-      alert("Lost");
-
-    // if (this.minesAround == 0)
-
-    this.updateDisplay();
+      this.lossEvent.emit(this.vertical + " " + this.horizontal);
+    else
+      this.uncoverEvent.emit(this.vertical + " " + this.horizontal);
   }
 
   handleRightClick(event : any)
@@ -73,11 +73,14 @@ export class TileComponent implements OnInit {
     event.preventDefault();
     if (this.uncovered)
       return;
-    this.flagState = (this.flagState + 1) % 3;
-    this.updateDisplay();
+    this.flagEvent.emit(this.vertical + " " + this.horizontal);
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      this.updateDisplay();
   }
 
 }
