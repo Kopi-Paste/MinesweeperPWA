9848ac85e6134bb14b98ea250939ce419a1d8c27
Vojtěch Kopal <kopalvo1@fit.cvut.cz>
2022-07-10 12:08:42 +0200

    Make tiles react to click

--- Minesweeper/src/app/tile/tile.component.ts
+++ Minesweeper/src/app/tile/tile.component.ts
@@ -1,3 +1,4 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
@@ -11,16 +12,69 @@ export class TileComponent implements OnInit {
 
   @Input() minesAround : number
 
+
+  uncovered : boolean;
+
+  flagState : number;
+
+  display : string;
+
   @Output() messageEvent = new EventEmitter<string>();
 
   constructor() {
     this.mine = false;
     this.minesAround = 0;
+    this.uncovered = false;
+    this.flagState = 0;
+    this.display = "_";
+   }
+
+   updateDisplay()
+   {
+      if (this.uncovered)
+        this.display = this.minesAround != 0 ? this.minesAround.toString() : "=";
+      else
+        switch (this.flagState)
+        {
+          case 0:
+            this.display = "_";
+            break;
+          case 1:
+            this.display = "F";
+            break;
+          case 2:
+            this.display = "?";
+            break;
+        }
    }
 
   sendMessage()
   {
     this.messageEvent.emit("Hey");
+  }
+
+  handleLeftClick()
+  {
+    if (this.uncovered || this.flagState == 1)
+      return;
+    this.flagState = 0;
+    this.uncovered = true;
+
+    if (this.mine)
+      alert("Lost");
+
+    // if (this.minesAround == 0)
+
+    this.updateDisplay();
+  }
+
+  handleRightClick(event : any)
+  {
+    event.preventDefault();
+    if (this.uncovered)
+      return;
+    this.flagState = (this.flagState + 1) % 3;
+    this.updateDisplay();
   }
 
   ngOnInit(): void {
